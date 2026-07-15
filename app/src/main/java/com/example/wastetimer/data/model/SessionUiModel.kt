5acc6f16aaf4557ecf00d4bbfe99dfd3a285a497
yuva-kiran-18 <yuvakirankumar7777@gmail.com>
2026-07-15 12:58:43 +0530

    Create SessionUiModel.kt

---
+++ app/src/main/java/com/example/wastetimer/data/model/SessionUiModel.kt
@@ -0,0 +1,13 @@
+package com.example.wastetimer.data.model
+
+data class SessionUiModel(
+
+    val sessionId: Long,
+
+    val startTime: Long,
+
+    val endTime: Long,
+
+    val duration: Long
+
+)
