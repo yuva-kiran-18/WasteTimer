31e84808bc69e1493c3a978f209d5b78d280253b
yuva-kiran-18 <yuvakirankumar7777@gmail.com>
2026-07-15 14:37:40 +0530

    Remove duplicate import of Icons

--- app/src/main/java/com/example/wastetimer/ui/history/components/EmptyHistory.kt
+++ app/src/main/java/com/example/wastetimer/ui/history/components/EmptyHistory.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
